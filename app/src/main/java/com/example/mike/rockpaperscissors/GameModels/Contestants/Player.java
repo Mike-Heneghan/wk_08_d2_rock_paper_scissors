1523b2ecb935c160f289ac4372a71baab1ca5c10
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-24 12:16:24 +0100

    Player and CPU subclasses done and Tested. CPU can choose random hand

--- app/src/main/java/com/example/mike/rockpaperscissors/GameModels/Contestants/Player.java
+++ app/src/main/java/com/example/mike/rockpaperscissors/GameModels/Contestants/Player.java
@@ -1,4 +1,8 @@
 package com.example.mike.rockpaperscissors.GameModels.Contestants;
 
-public class Player {
+public class Player extends Contestant {
+    public Player() {
+        super("Human");
+    }
+
 }
